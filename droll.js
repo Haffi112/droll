84cc21f85d429a98a989f4c0c19d1f05ac947768
Hafsteinn Einarsson <hafsteinneinarsson@Hafsteinns-MacBook-Pro.local>
2018-07-13 14:12:47 +0000

    Updated the parser.

--- droll.js
+++ droll.js
@@ -2,23 +2,28 @@
 
    "use strict";
 
+  function sortNumber(a,b) {
+    return a - b;
+  }
+
   var droll = {};
 
   // Define a "class" to represent a formula
   function DrollFormula() {
-    this.numDice   = 0;
-    this.numSides  = 0;
-    this.modifier  = 0;
-    
-    this.minResult = 0;
-    this.maxResult = 0;
-    this.avgResult = 0;
+    this.formula     = "";
+    this.numDice     = 0;
+    this.numSides    = 0;
+    this.modifier    = 0;
+    this.keepNum     = 0;
+    this.keepHL      = 0;
+    this.multFactor  = 0;
+    this.repeat      = 1;
   }
 
   // Define a "class" to represent the results of the roll
   function DrollResult() {
+    this.formula = "";
     this.rolls    = [];
-    this.modifier = 0;
     this.total    = 0;
   }
 
@@ -56,21 +61,41 @@
    * Returns a DrollFormula object on success or false on failure.
    */
   droll.parse = function(formula) {
-    var pieces = null;
-    var result = new DrollFormula();
+    var results = [];
+    //pieces = formula.match(/^([1-9]\d*)?d([1-9]\d*)([+-]\d+)?$/i);
+    var words = formula.split(" ");
+    //console.log(words);
+    for(var i=0; i < words.length; ++i) {
+      var word = words[i];
+      if(word == '+' || word == '-') {
+        results.push(word);
+        continue;
+      }
+      var result = new DrollFormula();
+      result.formula = word;
+      var pieces = null;
 
-    pieces = formula.match(/^([1-9]\d*)?d([1-9]\d*)([+-]\d+)?$/i);
-    if (!pieces) { return false; }
+      pieces = word.match(/^(([1-9]\d*)x)?\(?([1-9]\d*)?d([1-9]\d*)(K([1-9]\d*)([HL]))?(x([1-9]\d*))?([+-]\d+)?\)?$/i);
+      if (!pieces) { return false; }
 
-    result.numDice  = (pieces[1] - 0) || 1;
-    result.numSides = (pieces[2] - 0);
-    result.modifier = (pieces[3] - 0) || 0;
+      result.repeat = (pieces[2]-0) || 1;
+      result.numDice  = (pieces[3] - 0) || 1;
+      result.numSides = (pieces[4] - 0);
+      result.keepNum = (pieces[6] - 0) || result.numDice;
+      if(result.keepNum > result.numDice || result.keepNum < 0) {
+        console.log("You can only keep as many dice as you have!")
+        return false;
+      }
+      result.keepHL = pieces[7]
+      result.multFactor = (pieces[9] - 0) || 1;
+      result.modifier = (pieces[10] - 0) || 0;
 
-    result.minResult = (result.numDice * 1) + result.modifier;
-    result.maxResult = (result.numDice * result.numSides) + result.modifier;
-    result.avgResult = (result.maxResult + result.minResult) / 2;
+      //console.log(pieces);
 
-    return result;
+      results.push(result);
+    }
+    //console.log(results);
+    return results;
   };
 
   /**
@@ -78,7 +103,8 @@
    * Returns true on success or false on failure.
    */
   droll.validate = function(formula) {
-    return (droll.parse(formula)) ? true : false ;
+    var valid = (droll.parse(formula)) ? true : false;
+    return valid;
   };
 
   /**
@@ -87,23 +113,48 @@
    */
   droll.roll = function(formula) {
     var pieces = null;
-    var result = new DrollResult();
 
     pieces = droll.parse(formula);
     if (!pieces) { return false; }
 
-    for (var a=0; a<pieces.numDice; a++) {
-      result.rolls[a] = (1 + Math.floor(Math.random() * pieces.numSides));
+    var results = [];
+
+    for(var i = 0; i < pieces.length; ++i) {
+      if(pieces[i]=='+' || pieces[i]=='-') {
+        continue;
+      }
+      // Roll all the dice for this command
+      for(var k = 0; k < pieces[i].repeat; ++k) {
+        var result = new DrollResult();
+        result.formula = pieces[i].formula;
+        var dice = [];
+        for(var j = 0; j < pieces[i].numDice; ++j) {
+          dice.push((1 + Math.floor(Math.random() * pieces[i].numSides)));
+        }
+        dice.sort(sortNumber);
+        if(pieces[i].keepHL == 'H') {
+          dice.reverse();
+        }
+        var total = 0;
+        for(var j = 0; j < pieces[i].keepNum; ++j) {
+          total += dice[j];
+        }
+        total *= pieces[i].multFactor;
+        result.total = total + pieces[i].modifier;
+        if(i > 0 && pieces[i-1]=='-') {
+          result.total = -result.total;
+        }
+        result.rolls = dice;
+        results.push(result);
+      }
     }
 
-    result.modifier = pieces.modifier;
-
-    for (var b=0; b<result.rolls.length; b++) {
-      result.total += result.rolls[b];
+    var sum_total = 0;
+    for(var i = 0; i < results.length; ++i) {
+      sum_total += results[i].total;
     }
-    result.total += result.modifier;
-
-    return result;
+    console.log(results);
+    return sum_total;
   };
 
   // Export library for use in node.js or browser
